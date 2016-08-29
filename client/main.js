68543632d40179d1c8aa8d93e581b4a4ae5f9d43
Nazar <nazarbaker24@gmail.com>
2016-08-29 23:43:40 +0300

    add routing

--- client/main.js
+++ client/main.js
@@ -1,9 +1,25 @@
-import React from 'react'
 import { Meteor } from 'meteor/meteor'
+
+import React from 'react'
 import { render } from 'react-dom'
+import { mount } from 'react-mounter'
 
 import App from '../imports/ui/App.js'
+import Dashboard from '../imports/ui/dashboard/Dashboard.js'
+import Admin from '../imports/ui/admin/Admin.js'
 
-Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'))
-});
+FlowRouter.route("/", {
+  action() {
+    mount(App, {
+      content: (<Dashboard />)
+    })
+  }
+})
+
+FlowRouter.route("/admin", {
+  action() {
+    mount(App, {
+      content: (<Admin />)
+    })
+  }
+})
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
+
+// Tap Events Hack
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin();
 
 // App component - represents the whole app
 export default class App extends React.Component {
@@ -9,10 +14,13 @@ export default class App extends React.Component {
   render() {
     return (
       <MuiThemeProvider>
-        <AppBar
-            title="Title"
-        />
+        <div>
+          <AppBar
+              title="Title"
+          />
+          <div id="render-target">{ this.props.content }</div>
+        </div>
       </MuiThemeProvider>
-    );
+    )
   }
 }
